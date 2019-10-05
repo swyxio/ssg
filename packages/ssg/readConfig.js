6d918198b750438057c9dfd2415df059fdbe4279
sw-yx <swyx@dontemail.me>
2019-10-05 13:13:09 -0400

    fix readconfig bug when no ssg.config.js

--- packages/ssg/readConfig.js
+++ packages/ssg/readConfig.js
@@ -4,24 +4,33 @@ const fs = require('fs')
 
 // todo: actually use opts.ssgConfig
 const configPath = path.resolve(process.cwd(), 'ssg.config.js')
-console.log('ssg: reading config')
-const ssgConfig = require(configPath)
-const dotFolderPath = path.join('__sapper__', 'ssg')
-const dotFolderDataPath = path.join(dotFolderPath, 'data.json')
-const getIndex = () => JSON.parse(fs.readFileSync(dotFolderDataPath, 'utf8'))
 
-const getDataSlice = async (key, uid) => {
-  const plugins = ssgConfig.plugins
-  if (plugins) {
-    if (plugins[key]) {
-      return plugins[key].getDataSlice(uid)
+let getIndex = () => {
+  throw new Error('no ssg.config.js given so no index available')
+}
+let getDataSlice = () => {
+  throw new Error('no ssg.config.js given so no getDataSlice available')
+}
+if (fs.existsSync(configPath)) {
+  console.log('ssg: reading config')
+  const ssgConfig = require(configPath)
+  const dotFolderPath = path.join('__sapper__', 'ssg')
+  const dotFolderDataPath = path.join(dotFolderPath, 'data.json')
+  getIndex = () => JSON.parse(fs.readFileSync(dotFolderDataPath, 'utf8'))
+
+  getDataSlice = async (key, uid) => {
+    const plugins = ssgConfig.plugins
+    if (plugins) {
+      if (plugins[key]) {
+        return plugins[key].getDataSlice(uid)
+      }
     }
+    if (ssgConfig.getDataSlice) {
+      return ssgConfig.getDataSlice(key, uid)
+    }
+    // fallback
+    throw new Error('no data found for key: ' + key + ' uid: ' + uid)
   }
-  if (ssgConfig.getDataSlice) {
-    return ssgConfig.getDataSlice(key, uid)
-  }
-  // fallback
-  throw new Error('no data found for key: ' + key + ' uid: ' + uid)
 }
 module.exports = {
   getDataSlice,
