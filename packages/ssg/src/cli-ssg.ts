93bc717bd198ac0b5b7b2f6afe8cbeb7dbf93263
sw-yx <swyx@swyx.io>
2019-10-31 00:37:36 -0400

    fix the zeroconfig experience to make it run data properly

--- packages/ssg/src/cli-ssg.ts
+++ packages/ssg/src/cli-ssg.ts
@@ -1,33 +1,37 @@
-import chokidar from 'chokidar'
-import fs from 'fs'
-import path from 'path'
-import { ensureDirectoryExistence } from './utils'
-import coreData from './coreData'
-type Dict = Record<string, any>
+import chokidar from 'chokidar';
+import fs from 'fs';
+import path from 'path';
+import { ensureDirectoryExistence } from './utils';
+import coreData from './coreData';
+type Dict = Record<string, any>;
 type SSGConfig = {
   /** space separated places to watch for reloads. default 'content' */
-  watchFolders: string
-  configPath: string
-  plugins: Dict
-  createIndex(mainIndex: Dict): Promise<{ [key: string]: any }>
-  postExport: (index: { [key: string]: any }) => void
-  coreDataOpts: any
-}
+  watchFolders: string;
+  configPath: string;
+  plugins: Dict;
+  createIndex(mainIndex: Dict): Promise<{ [key: string]: any }>;
+  postExport: (index: { [key: string]: any }) => void;
+  coreDataOpts: any;
+};
 
 /**
  *
  * run getInitialData only once
  *
  */
-export async function getSSGDataOnce(ssgConfig: SSGConfig, sapperDir: string) {
-  let mainIndex = {} as { [key: string]: any }
-  const plugins = ssgConfig.plugins
-  if (plugins.ssgCoreData) throw new Error('plugin named ssgCoreData found, this is a reserved name')
+export async function getSSGDataOnce(
+  ssgConfig?: SSGConfig,
+  sapperDir: string = process.cwd()
+) {
+  let mainIndex = {} as { [key: string]: any };
+  const plugins = ssgConfig && ssgConfig.plugins;
+  if (plugins && plugins.ssgCoreData)
+    throw new Error('plugin named ssgCoreData found, this is a reserved name');
   if (plugins) {
     // todo: parallelize
     for (let temp of Object.entries(plugins)) {
-      const [pluginName, plugin] = temp
-      mainIndex[pluginName] = await plugin.createIndex()
+      const [pluginName, plugin] = temp;
+      mainIndex[pluginName] = await plugin.createIndex();
     }
   }
 
@@ -36,26 +40,26 @@ export async function getSSGDataOnce(ssgConfig: SSGConfig, sapperDir: string) {
    */
   // TODO: understand input folder/file/glob
   // TODO: exempt ignored files
-  const coreDataPlugin = coreData(ssgConfig.coreDataOpts)
-  mainIndex.ssgCoreData = coreDataPlugin.createIndex()
+  const coreDataPlugin = coreData(ssgConfig && ssgConfig.coreDataOpts);
 
-  if (ssgConfig.createIndex) {
-    mainIndex = await ssgConfig.createIndex(mainIndex)
-    const dotFolderPath = path.join(sapperDir, 'ssg')
-    const dotFolderDataPath = path.join(dotFolderPath, 'data.json')
-    ensureDirectoryExistence(dotFolderDataPath)
-    if (!fs.existsSync(dotFolderPath)) fs.mkdirSync(dotFolderPath)
-    fs.writeFileSync(dotFolderDataPath, JSON.stringify(mainIndex))
-    return mainIndex
+  if (ssgConfig && ssgConfig.createIndex) {
+    mainIndex = await ssgConfig.createIndex(mainIndex);
   }
+  mainIndex.ssgCoreData = await coreDataPlugin.createIndex();
+  const dotFolderPath = path.join(sapperDir, 'ssg');
+  const dotFolderDataPath = path.join(dotFolderPath, 'data.json');
+  ensureDirectoryExistence(dotFolderDataPath);
+  if (!fs.existsSync(dotFolderPath)) fs.mkdirSync(dotFolderPath);
+  fs.writeFileSync(dotFolderDataPath, JSON.stringify(mainIndex));
+  return mainIndex;
 
-  // idk if this is the best check...
-  if (Object.keys(mainIndex).length < 1) {
-    console.warn('ssg warning: no index data from ssg plugins found, continuing as sapper app')
-    return null
-  } else {
-    return mainIndex
-  }
+  // // idk if this is the best check...
+  // if (Object.keys(mainIndex).length < 1) {
+  //   console.warn('ssg warning: no index data from ssg plugins found, continuing as sapper app')
+  //   return null
+  // } else {
+  //   return mainIndex
+  // }
 }
 
 /**
@@ -63,15 +67,19 @@ export async function getSSGDataOnce(ssgConfig: SSGConfig, sapperDir: string) {
  * read ssg config and ensure defaults exist
  *
  */
-export function readSSGConfig(ssgConfigPath: string): SSGConfig | null {
+export function readSSGConfig(ssgConfigPath: string): SSGConfig | undefined {
   if (!fs.existsSync(ssgConfigPath)) {
-    console.warn('ssgConfig file ' + ssgConfigPath + ' doesnt exist, continuing as regular sapper app')
-    return null
+    console.warn(
+      'ssgConfig file ' +
+        ssgConfigPath +
+        ' doesnt exist, continuing as regular sapper app'
+    );
+    return;
   }
-  let ssgConfig = require(path.resolve(ssgConfigPath))
-  ssgConfig.configPath = ssgConfigPath
-  ssgConfig.watchFolders = ssgConfig.watchFolders || 'content'
-  return ssgConfig
+  let ssgConfig = require(path.resolve(ssgConfigPath));
+  ssgConfig.configPath = ssgConfigPath;
+  ssgConfig.watchFolders = ssgConfig.watchFolders || 'content';
+  return ssgConfig;
 }
 
 /**
@@ -80,26 +88,31 @@ export function readSSGConfig(ssgConfigPath: string): SSGConfig | null {
  *
  */
 export function watchSSGFiles(watcher: any, ssgConfig: Partial<SSGConfig>) {
-  let isReady = false
+  let isReady = false;
   const watchHandler = (event: string) => async (path: string) => {
     // bypass the initial 'add' events
-    if (event === 'started') isReady = true
-    else if (!isReady) return
+    if (event === 'started') isReady = true;
+    else if (!isReady) return;
 
     // cue the restart message e.g. `content/color.yml changed. rebuilding...`
-    watcher.restart(path, 'client') // not sure if 'client'
+    watcher.restart(path, 'client'); // not sure if 'client'
     // get the frontend to live reload!
-    watcher.dev_server.send({ action: 'reload' })
-  }
-  const filesToWatch = [...ssgConfig.watchFolders!.split(' '), ssgConfig.configPath].filter(Boolean) as string[]
+    watcher.dev_server.send({ action: 'reload' });
+  };
+  const filesToWatch = [
+    ...ssgConfig.watchFolders!.split(' '),
+    ssgConfig.configPath
+  ].filter(Boolean) as string[];
   if (filesToWatch.length < 1) {
-    console.log('Warning: no SSG config or content files detected, operating as a basic Sapper app!')
-    return
+    console.log(
+      'Warning: no SSG config or content files detected, operating as a basic Sapper app!'
+    );
+    return;
   }
-  const chokiwatch = chokidar.watch(filesToWatch)
+  const chokiwatch = chokidar.watch(filesToWatch);
   chokiwatch
     .on('add', watchHandler('added'))
     .on('change', watchHandler('changed'))
-    .on('error', (error) => console.log(`chokiwatch error: ${error}`))
-    .on('ready', watchHandler('started'))
+    .on('error', error => console.log(`chokiwatch error: ${error}`))
+    .on('ready', watchHandler('started'));
 }
--- packages/ssg/src/coreData.ts
+++ packages/ssg/src/coreData.ts
@@ -1,26 +1,26 @@
-var unified = require('unified')
+var unified = require('unified');
 // var stream = require('unified-stream')
-var vfile = require('to-vfile')
-var report = require('vfile-reporter')
-const { produce } = require('immer')
+var vfile = require('to-vfile');
+var report = require('vfile-reporter');
+const { produce } = require('immer');
 
-const { promisify } = require('util')
-const fs = require('fs')
-const path = require('path')
-const readdir = promisify(fs.readdir)
-const stat = promisify(fs.stat)
+const { promisify } = require('util');
+const fs = require('fs');
+const path = require('path');
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
 // const lstat = promisify(fs.lstat)
-const frontMatter = require('front-matter')
+const frontMatter = require('front-matter');
 
-const tob64 = (str: string) => Buffer.from(str).toString('base64')
-const fromb64 = (str: string) => Buffer.from(str, 'base64').toString()
+const tob64 = (str: string) => Buffer.from(str).toString('base64');
+const fromb64 = (str: string) => Buffer.from(str, 'base64').toString();
 /**
  *
  * globals, we may have to make per-invocation in future
  *
  */
 
-let _recognizedExtensions = ['.md', '.markdown', '.mdx', '.svexy']
+let _recognizedExtensions = ['.md', '.markdown', '.mdx', '.svexy'];
 let _preset = {
   settings: {},
   plugins: [
@@ -34,9 +34,9 @@ let _preset = {
           type: 'element',
           tagName: 'span',
           properties: { className: ['icon', 'icon-link'] },
-          children: [{ type: 'text', value: ' 🔗' }],
-        },
-      },
+          children: [{ type: 'text', value: ' 🔗' }]
+        }
+      }
     ],
     require('remark-toc'),
     require('remark-sectionize'),
@@ -44,113 +44,130 @@ let _preset = {
     require('rehype-format'),
     [require('remark-frontmatter'), ['yaml']],
     [require('rehype-shiki'), { theme: 'Material-Theme-Palenight' }],
-    require('rehype-stringify'),
-  ],
-}
+    require('rehype-stringify')
+  ]
+};
 
 export type PluginOpts = {
-  dirPath?: string
-  dateFilterType: 'all' | 'current' | undefined
-  modifyRecognizedExtensions?: string
-  modifyRemarkConfig?: string
-  ignoreGlobs?: string[]
+  dirPath?: string;
+  dateFilterType?: 'all' | 'current';
+  modifyRecognizedExtensions?: string;
+  modifyRemarkConfig?: string;
+  ignoreGlobs?: string[];
   // onCreateIndex?: (index: {
   //     [slug: string]: SSGRemarkPluginFile;
   // }) => Promise<void>
-}
-type SSGRemarkPluginFile = { uid: string; createdAt: Date; modifiedAt: Date; metadata: any }
-export default function(opts: PluginOpts) {
+};
+type SSGRemarkPluginFile = {
+  uid: string;
+  createdAt: Date;
+  modifiedAt: Date;
+  metadata: any;
+};
+export default function(opts?: PluginOpts) {
   // console.warn('dirpath not supplied to coreDataOpts, using default')
-  let _dirPath = opts.dirPath || '.'
+  if (opts === null || opts === undefined) opts = {};
+  let _dirPath = opts.dirPath || path.resolve('.');
   if (opts.modifyRecognizedExtensions) {
-    _recognizedExtensions = produce(_recognizedExtensions, opts.modifyRecognizedExtensions)
+    _recognizedExtensions = produce(
+      _recognizedExtensions,
+      opts.modifyRecognizedExtensions
+    );
   }
   if (opts.modifyRemarkConfig) {
-    _preset = produce(_preset, opts.modifyRemarkConfig)
+    _preset = produce(_preset, opts.modifyRemarkConfig);
   }
 
   // flattens all directories below the dirPath
   // is recursive!
   async function createIndex(recursiveDir = _dirPath) {
-    const files = await readdir(recursiveDir)
+    const files = await readdir(recursiveDir);
     const getStats = async (file: string, _dirPath: string) => {
-      const filePath = path.join(_dirPath, file)
-      const st = await stat(filePath)
+      const filePath = path.join(_dirPath, file);
+      const st = await stat(filePath);
       if (st.isDirectory()) {
-        if ((file = 'node_modules')) return // dont go inside node_modules
-        const temp = await createIndex(filePath) // recursion
-        return Object.values(temp) // take it back out of an object into an array
+        if ((file = 'node_modules')) return; // dont go inside node_modules
+        const temp = await createIndex(filePath); // recursion
+        return Object.values(temp); // take it back out of an object into an array
       } else {
-        if (file === '.DS_Store') return // skip ds store...
-        if (!_recognizedExtensions.includes(path.extname(file))) return // skip
+        if (file === '.DS_Store') return; // skip ds store...
+        if (!_recognizedExtensions.includes(path.extname(file))) return; // skip
         return [
           {
             uid: tob64(filePath),
             createdAt: st.birthtime,
-            modifiedAt: st.mtime,
-          },
-        ]
+            modifiedAt: st.mtime
+          }
+        ] as SSGRemarkPluginFile[];
       }
-    }
-    const arrs: (SSGRemarkPluginFile[])[] = await Promise.all(files.map((file: string) => getStats(file, recursiveDir)))
-    const strArr = [] as SSGRemarkPluginFile[]
-    let index = strArr.concat.apply([], arrs) // ghetto flatten
+    };
+    const promises: Promise<SSGRemarkPluginFile[]>[] = files.map(
+      (file: string) => getStats(file, recursiveDir)
+    );
+    const arrs: (SSGRemarkPluginFile[])[] = await Promise.all(promises);
+    const strArr = [] as SSGRemarkPluginFile[];
+    let index = strArr.concat.apply([], arrs); // ghetto flatten
     index = index
       .filter(Boolean)
-      .map((file) => {
-        const temp = fs.readFileSync(fromb64(file.uid), 'utf-8')
-        const { attributes: metadata } = frontMatter(temp)
-        if (!metadata) return // require metadata
-        if (!metadata.title) return // require title
-        if (metadata.published === false) return // if published is false
-        let pubdate = metadata.date || new Date().toString().slice(4, 15)
-        const date = new Date(`${pubdate} EDT`) // cheeky hack
-        metadata.pubdate = pubdate
-        metadata.date = new Date(pubdate)
-        metadata.dateString = date.toDateString()
-        file.metadata = metadata
-        return file
+      .map(file => {
+        const temp = fs.readFileSync(fromb64(file.uid), 'utf-8');
+        const { attributes: metadata } = frontMatter(temp);
+        // if (!metadata) return; // require metadata
+        // if (!metadata.title) return; // require title
+        if (metadata.published === false) return; // if published is false
+        let pubdate = metadata.date || new Date().toString().slice(4, 15);
+        const date = new Date(`${pubdate} EDT`); // cheeky hack
+        metadata.pubdate = pubdate;
+        metadata.date = new Date(pubdate);
+        metadata.dateString = date.toDateString();
+        file.metadata = metadata;
+        return file;
       })
-      .filter(notEmpty)
-      .sort((a, b) => {
-        return a!.metadata.pubdate < b!.metadata.pubdate ? 1 : -1
-      })
-      .filter((x) => (opts.dateFilterType === 'all' ? true : new Date(x!.metadata.date) <= new Date()))
-    const result = extractSlugObjectFromArray(index)
+      .filter(notEmpty);
+    // .sort((a, b) => {
+    //   return a!.metadata.pubdate < b!.metadata.pubdate ? 1 : -1;
+    // })
+    // .filter(x =>
+    //   opts.dateFilterType === 'all'
+    //     ? true
+    //     : new Date(x!.metadata.date) <= new Date()
+    // );
+    // const result = extractSlugObjectFromArray(index);
+    const result = index;
     // // i dont really use this yet
     // if (opts.onCreateIndex) {
     //   await opts.onCreateIndex(result) // optional logging
     // }
-    return result
+    return result;
   }
 
   // https://stackoverflow.com/questions/43118692/typescript-filter-out-nulls-from-an-array
   function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
-    return value !== null && value !== undefined
+    return value !== null && value !== undefined;
   }
 
   async function getDataSlice(uid: string) {
-    const filepath = fromb64(uid)
-    const md = vfile.readSync(filepath)
+    const filepath = fromb64(uid);
+    const md = vfile.readSync(filepath);
     const file = await unified()
       .use(_preset)
       .process(md)
       .catch((err: Error) => {
-        console.error(report(md))
-        throw err
-      })
-    file.extname = '.html'
-    return file.toString()
+        console.error(report(md));
+        throw err;
+      });
+    file.extname = '.html';
+    return file.toString();
   }
 
   return {
     createIndex,
-    getDataSlice,
-  }
+    getDataSlice
+  };
 }
 
-function extractSlugObjectFromArray(arr: SSGRemarkPluginFile[]) {
-  let obj = {} as { [slug: string]: SSGRemarkPluginFile }
-  arr.forEach((item) => (obj[item.metadata.slug] = item))
-  return obj
-}
+// function extractSlugObjectFromArray(arr: SSGRemarkPluginFile[]) {
+//   let obj = {} as { [slug: string]: SSGRemarkPluginFile };
+//   arr.forEach(item => (obj[item.metadata.slug] = item));
+//   return obj;
+// }
